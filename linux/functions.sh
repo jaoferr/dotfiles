139b8c691b32a6d2aeacbe068ac0fd5966ecea26
João Fernando <60816627+jaoferr@users.noreply.github.com>
2023-09-23 12:51:41 -0300

    Added useenv function

--- linux/functions.sh
+++ linux/functions.sh
@@ -21,3 +21,35 @@ else
     echo "no web container running or command not available"
 fi
 }
+
+# activates a python virtual env
+# https://github.com/LunarVim/LunarVim/issues/1357#issuecomment-906758577
+useenv() {
+  local OLD_PATH=$PWD
+  local VENV_DIR=${1:-'.venv'} #default venv name
+  local FOUND
+
+  while [ -z "$FOUND" ] ; do
+    if ls ${VENV_DIR}/bin/activate 2>/dev/null >/dev/null ; then
+      source ${VENV_DIR}/bin/activate
+      FOUND='FOUND'
+      break
+    fi
+
+    if [ "/home/$USER" == "$PWD" ] || [ '/' == "$PWD" ] ; then
+      break
+    fi
+
+    cd ..
+  done
+
+  if [ -z "$FOUND" ] ;then
+    echo "venv not found"
+  else
+    VERSION=`python3 --version 2>&1`
+    printf 'Using %s\n' "$VERSION"
+    echo found on $PWD
+  fi
+
+  cd $OLD_PATH
+}
